b1430c558ecbf84e8c3cce934da33a461068c4b0
aditya-ast <110805338+aditya-ast@users.noreply.github.com>
2023-04-25 04:51:24 +0000

    commit from Aditya

---
+++ src/pages/userlist.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import { Avatar } from '@mui/material';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
+
+export default function Userlist() {
+  return (
+    <Paper
+      sx={{
+        p: 1,
+        margin: 'auto',
+        maxWidth: 1000,
+        flexGrow: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+      }}
+    >
+      <Grid container spacing={1}>
+        <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+            <Avatar sx={{ width: 85, height: 85 }} alt="complex" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" ></Avatar>
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={10} sm container>
+          <Grid item xs container direction="column" spacing={1}>
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                Standard license
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                Full resolution 1920x1080 • JPEG
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                ID: 1030114
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                Remove
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              $19.00
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
